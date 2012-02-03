bab63bd9dca815f397e129f275e3ffc98485e6f4
Carl <hndeng06@gmail.com>
2012-02-03 14:08:03 +0800

    add some to run.sh

--- w3cam-0.7.2/run.sh
+++ w3cam-0.7.2/run.sh
@@ -3,6 +3,7 @@
 #after setup the http server, run http://192.168.2.234/cgi-bin/w3cam.cgi you will see a 
 #pic in web browse.
 #
+#
 http://192.168.2.234/cgi-bin/hello-cgi
 
 http://192.168.2.234/cgi-bin/w3cam.cgi?width=240&height=180&mode=gui&quality=65&usleep=20000&input=-1&format=JPEG&size=240x180&refresh=-1
