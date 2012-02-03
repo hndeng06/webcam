075fa2718e4014825b60063e9914714cdaebc683
Carl <hndeng06@gmail.com>
2012-02-03 13:49:40 +0800

    update run.sh

--- w3cam-0.7.2/run.sh
+++ w3cam-0.7.2/run.sh
@@ -1,5 +1,11 @@
+#################################run instruction##################################
+#w3cam program need a http web server,and a input yuv420p file.
+#after setup the http server, run http://192.168.2.234/cgi-bin/w3cam.cgi you will see a 
+#pic in web browse.
+#
 http://192.168.2.234/cgi-bin/hello-cgi
 
 http://192.168.2.234/cgi-bin/w3cam.cgi?width=240&height=180&mode=gui&quality=65&usleep=20000&input=-1&format=JPEG&size=240x180&refresh=-1
 
 http://192.168.2.234/cgi-bin/w3cam.cgi
+
